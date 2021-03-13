92a3109ef6a2a9b015ab4f8d21d889c4d78dc8b5
Chi Vong <chivongv@gmail.com>
2021-03-13 19:38:19 +0100

    🚧 WIP FrontPage 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,59 @@
+import styled from '@emotion/styled';
+
+const Container = styled('header')({
+  display: 'flex',
+  position: 'fixed',
+  padding: '10px 15px',
+  zIndex: 1001,
+  background: '#fff',
+  width: '100%',
+  justifyContent: 'space-between',
+});
+
+const Nav = styled('nav')({
+  position: 'relative',
+});
+
+const LinkList = styled('ul')({
+  listStyle: 'none',
+  display: 'flex',
+});
+
+const Link = styled('li')({
+  marginRight: 10,
+  ':last-child': {
+    marginRight: 0,
+  },
+});
+
+const Logo = styled('a')({
+  padding: 8,
+  '> img': {
+    width: 80,
+  },
+});
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Logo>
+        <img src="/logo.png" />
+      </Logo>
+      <Nav>
+        <LinkList>
+          <Link>
+            <a href="/our-offers">What we do</a>
+          </Link>
+          <Link>
+            <a href="/case">Info</a>
+          </Link>
+          <Link>
+            <a href="/partner-1">Eng</a>
+          </Link>
+        </LinkList>
+      </Nav>
+    </Container>
+  );
 };
 
 export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
-import Head from "next/head";
-import Navbar from "@components/Navbar";
+import Head from 'next/head';
+import styled from '@emotion/styled';
+import Navbar from '@components/Navbar';
+import Hero from '@components/Hero';
+import Services from '@components/Services';
+import Cases from '@components/Cases';
+import Customers from '@components/Customers';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
     <div>
+      <Head></Head>
       <Navbar />
+      <Hero />
+      <Services />
+      <Cases />
+      <Customers />
+      <Footer />
     </div>
   );
 }
